e9b50ec03d6b043619e78f5d5ad177d1a4dcfd76
Abiud Sokomi <abiudsokomi@gmail.com>
2023-11-21 06:45:07 +0300

    my commit

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 var password = "!@Abiud123";
 var response;
+var entryCount = 0;
+var entryLimit = 3;
+var error = false;
 
-while (response != password) {
-    response = window.prompt("Enter password");
-}
-alert("You got it");
+    while (response != password && !error) {
+        if (entryCount < entryLimit) {
+            response = window.prompt("Enter password");
+            entryCount++; 
+        } else {
+            error = true;
+        }   
+    } 
+ if (error) {
+    alert("Too many entries");
+ }  else {
+    alert("You got it");
+ }
